5657ba3e761edbf039c85a5609726adc368abdff
antoanelenkov <antoanelenkov@gmail.com>
2015-09-28 17:20:40 +0300

    Added 'start' command in the messages.

--- Source/Common/Constants/GlobalMessages.cs
+++ Source/Common/Constants/GlobalMessages.cs
@@ -9,7 +9,7 @@ namespace BalloonsPop.Common
     public class GlobalMessages
     { 
         public const string ALREADY_POPPED_BALLOON_MSG = "You cannot pop already poppped balloon";
-        public const string GREETING_MSG = "Welcome to “Balloons Pops” game. Please try to pop the balloons. Use:\n'top' to view the top scoreboard\n'restart' to start a new game\n'exit' to quit the game.";
+        public const string GREETING_MSG = "Welcome to “Balloons Pops” game. Please try to pop the balloons. Write:\n'start' for starting a new game\n'top' to view the top scoreboard\n'restart' to restart the game\n'exit' to quit the game.\n";
         public const string ROW_COW_MSG = "Enter a row and column: ";
         public const string INVALID_COMMAND_MSG = "Invalid move or command!";
         public const string ADD_TO_TOPSCORE_MSG = "Please enter your name for the top scoreboard: ";

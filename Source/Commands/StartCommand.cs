0e7fea1f47180e42dc5e8a7c6a097a9bb5482c09
antoanelenkov <antoanelenkov@gmail.com>
2015-09-29 15:04:47 +0300

    Easy, Medium, Hard levels added.

--- Source/Commands/StartCommand.cs
+++ Source/Commands/StartCommand.cs
@@ -13,7 +13,26 @@
 
         public void Execute()
         {
-            //TODO: add logic for user input of rows and cols form gamefield
+            this.Context.Printer.PrintMessage("How dificult do you want it: Your options are:\neasy\nmedium\nhard");
+            var input=this.Context.Reader.ReadInput();
+            GameField gamefield;
+
+            if (input == "easy")
+            {
+                gamefield = new GameField(5, 5);
+                this.Context.GameLogic.Game = new Game(gamefield);
+            }
+            else if(input == "medium")
+            {
+                gamefield = new GameField(8, 8);
+                this.Context.GameLogic.Game = new Game(gamefield);
+            }
+            else if (input == "hard")
+            {
+                gamefield = new GameField(10, 10);
+                this.Context.GameLogic.Game = new Game(gamefield);
+            }      
+
             this.Context.Printer.PrintGameBoard(this.Context.GameLogic.Game.Field);
         }
     }

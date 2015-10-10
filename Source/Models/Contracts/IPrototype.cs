108f35e418059878dfd07ba82accd0748586a1d5
AYankova <antonina.yankova@gmail.com>
2015-10-10 20:05:26 +0300

    IPrototype, GameField documented

--- Source/Models/Contracts/IPrototype.cs
+++ Source/Models/Contracts/IPrototype.cs
@@ -1,7 +1,21 @@
-﻿namespace BalloonsPop.Models.Contracts
+﻿//-----------------------------------------------------------------------
+// <copyright file="IPrototype.cs" company="Baloons-Pop-Three">
+//    Copyright Baloons-Pop-Three. All rights reserved
+// </copyright>
+// <summary>This is the IPrototype interface.</summary>
+//-----------------------------------------------------------------------
+namespace BalloonsPop.Models.Contracts
 {
+    /// <summary>
+    /// Interface that provides a method for cloning a class.
+    /// </summary>
+    /// <typeparam name="T">T is a class.</typeparam>
     public interface IPrototype<T> where T : class
     {
+        /// <summary>
+        /// Clones the class's instance.
+        /// </summary>
+        /// <returns>Cloned instance of the same class.</returns>
         T Clone();
     }
 }

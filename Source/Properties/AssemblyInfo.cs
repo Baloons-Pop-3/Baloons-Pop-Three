a740b5abbcabd52ef4eae56ed4fb1d5d8d3c4f10
antoanelenkov <antoanelenkov@gmail.com>
2015-10-10 13:58:11 +0300

    fixed AssemblyInfo.cs

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BalloonsPop.Properties")]
+[assembly: AssemblyTitle("BalloonsPop")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -31,3 +31,4 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("BalloonsPop.Tests" )]

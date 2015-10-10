e0dd3a4f12b011eb61349a9764a3445334097634
antoanelenkov <antoanelenkov@gmail.com>
2015-10-11 00:10:43 +0300

    SaveCommandTests.cs tests added

---
+++ Source/BalloonsPop.Tests/Commands.Tests/SaveCommandTests.cs
@@ -0,0 +1,31 @@
+﻿namespace BalloonsPop.Tests.Commands.Tests
+{
+    using BalloonsPop.Commands;
+    using Mocks;
+    using Contexts.Contracts;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class SaveCommandTests
+    {
+        private IContext context;
+        private readonly SaveCommand command;
+
+        public SaveCommandTests()
+        {
+            this.context = new MockIContext().mockContext.Object;
+            this.command = new SaveCommand();
+        }
+
+        [TestMethod]
+        public void Execute_ShouldNotThrow()
+        {
+            this.command.Execute(this.context);
+        }
+    }
+}

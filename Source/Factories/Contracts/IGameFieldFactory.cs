de6df99197d1470c9a0674f0fdef623184217274
AYankova <antonina.yankova@gmail.com>
2015-10-10 21:25:14 +0300

    GameFactory and IGameFieldFactory documented

--- Source/Factories/Contracts/IGameFieldFactory.cs
+++ Source/Factories/Contracts/IGameFieldFactory.cs
@@ -1,12 +1,24 @@
-﻿namespace BalloonsPop.Factories.Contracts
+﻿//-----------------------------------------------------------------------
+// <copyright file="IGameFieldFactory.cs" company="Baloons-Pop-Three">
+//    Copyright Baloons-Pop-Three. All rights reserved
+// </copyright>
+// <summary>This is the IGameFieldFactory interface.</summary>
+//-----------------------------------------------------------------------
+namespace BalloonsPop.Factories.Contracts
 {
-    using BalloonsPop.Commands.Contracts;
     using BalloonsPop.Common.Enums;
-    using BalloonsPop.Contexts.Contracts;
     using Models;
 
+    /// <summary>
+    /// Interface that provides a method for creating a game.
+    /// </summary>
     internal interface IGameFieldFactory
     {
+        /// <summary>
+        /// Creates a new game.
+        /// </summary>
+        /// <param name="input">The level of difficulty - easy, medium, hard, torture.</param>
+        /// <returns>New game with the selected level of difficulty.</returns>
         Game CreateGame(GameDifficulty input);
     }
 }
--- Source/Factories/GameFactory.cs
+++ Source/Factories/GameFactory.cs
@@ -1,14 +1,27 @@
-﻿namespace BalloonsPop.Factories
+﻿//-----------------------------------------------------------------------
+// <copyright file="GameFactory.cs" company="Baloons-Pop-Three">
+//    Copyright Baloons-Pop-Three. All rights reserved
+// </copyright>
+// <summary>This is the GameFactory class.</summary>
+//-----------------------------------------------------------------------
+namespace BalloonsPop.Factories
 {
     using System;
     using Common.Constants;
     using Common.Enums;
-    using Contexts.Contracts;
     using Contracts;
     using Models;
 
+    /// <summary>
+    /// Class that provides a method for creating a game.
+    /// </summary>
     internal class GameFactory : IGameFieldFactory
     {
+        /// <summary>
+        /// Creates a new game.
+        /// </summary>
+        /// <param name="input">The level of difficulty - easy, medium, hard, torture.</param>
+        /// <returns>New game with the selected level of difficulty.</returns>
         public Game CreateGame(GameDifficulty input)
         {
             switch (input)

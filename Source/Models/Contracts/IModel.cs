12afce4a9cfbac9df849587a7e0841717339ece3
AYankova <antonina.yankova@gmail.com>
2015-10-10 19:20:50 +0300

    IModel, OPlayer, Player documented

--- Source/Models/Contracts/IModel.cs
+++ Source/Models/Contracts/IModel.cs
@@ -1,7 +1,20 @@
-﻿namespace BalloonsPop.Models.Contracts
+﻿//-----------------------------------------------------------------------
+// <copyright file="IModel.cs" company="Baloons-Pop-Three">
+//    Copyright Baloons-Pop-Three. All rights reserved
+// </copyright>
+// <summary>This is the IModel interface.</summary>
+//-----------------------------------------------------------------------
+namespace BalloonsPop.Models.Contracts
 {
+    /// <summary>
+    /// Interface that provides property for getting and setting the id of a model.
+    /// </summary>
     public interface IModel
     {
+        /// <summary>
+        /// Gets or sets the id of the model.
+        /// </summary>
+        /// <value>The id of the model.</value>
         string Id { get; set; }
     }
 }

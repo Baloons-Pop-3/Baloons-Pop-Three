e37cc89bceb60f96cd51ce64747abfcf87add07a
AYankova <antonina.yankova@gmail.com>
2015-10-10 22:05:05 +0300

    EntryPoint and GameFacade documented

--- Source/EntryPoint.cs
+++ Source/EntryPoint.cs
@@ -1,9 +1,21 @@
-﻿namespace BalloonsPop
+﻿//-----------------------------------------------------------------------
+// <copyright file="EntryPoint.cs" company="Baloons-Pop-Three">
+//    Copyright Baloons-Pop-Three. All rights reserved
+// </copyright>
+// <summary>This is the EntryPoint class.</summary>
+//-----------------------------------------------------------------------
+namespace BalloonsPop
 {
+    /// <summary>
+    /// Class that provides an entry point for the game.
+    /// </summary>
     public class EntryPoint
     {
-        private static void Main(string[] args)
-        {
+        /// <summary>
+        /// Starts the game.
+        /// </summary>
+        private static void Main()
+        {         
             GameFacade game = new GameFacade();
 
             game.Start();
